c18be2016e7beeae6f42e8517c2ca418f3fadf40
jeseg <jeseg@LAPTOP-IE8SSJC8>
2021-03-15 10:24:32 +0100

    Cafe test

---
+++ PracticaDSS/src/test/java/com/jedago/practica_dss/tests/testCafe.java
@@ -0,0 +1,24 @@
+package com.jedago.practica_dss.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testCafe {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
