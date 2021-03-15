d4f421ab2adef614da0d0bd7b19a41fa238b39a5
David <David@LAPTOP-73DGTKH9>
2021-03-15 10:31:52 +0100

    IProduct

--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/IProduct.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/IProduct.java
@@ -1,5 +1,14 @@
 package com.jedago.practica_dss.core;
 
-interface IProduct {
-
+public interface IProduct {
+	private static
+	
+	public String  getID();
+	public String  getName();
+	public double  getPriceUnit();
+	public int 	   getStock();
+	public double  setPriceUnit(double price_);
+	public int 	   setStock(int stock_);
+	
+	
 }
--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/Product.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/Product.java
@@ -1,5 +1,24 @@
 package com.jedago.practica_dss.core;
 
 class Product implements IProduct {
-
+	private
+	
+	public String getID() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public double  getPriceUnit() {
+			return priceunit;
+	}
+	public int 	  getStock() {
+		return stock;
+	}
+	public double  setPriceUnit(double price_) {
+		this.price = price_;
+	}
+	public int 	  setStock(int stock_) {
+		this.stock = stock_;
+	}
 }
