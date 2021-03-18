3a70a8b353d7b66b4414e970d21901c6a90daf62
jeseg <jeseg@LAPTOP-IE8SSJC8>
2021-03-18 11:38:26 +0100

    equls de product

--- PracticaDSS/src/test/java/com/jedago/practica_dss/test/testCafe.java
+++ PracticaDSS/src/test/java/com/jedago/practica_dss/test/testCafe.java
@@ -16,6 +16,7 @@ public class testCafe {
 	private List<Product> lista_productos;
 	private Product p1;
 	private Cafe C;
+	private Order o;
 
 	@Before
 	public void setUp() throws Exception {
@@ -28,13 +29,54 @@ public class testCafe {
 
 	@After
 	public void tearDown() throws Exception {
+		lista_pedidos = null;
+		lista_productos = null;
+		p1 = null;
+		C = null;
 	}
 
 	@Test
-	public void constructor() {
+	public void testConstructor() {
 		//Crear el cafe
-		
-		//Comprobar que el cafe no sea nulo
+		C = new Cafe(lista_pedidos, lista_productos);
+		assert(C != null);
 	}
-
+	
+	@Test
+	public void testShowProducts() {
+		//Comprobar que saque por pantalla los productos disponibles
+		//??
+	}
+	
+	@Test
+	public void testShowCashBox() {
+		//Comprobar que muestre por pantalla el número de pedidos registrados y el dinero total
+		//??
+	}
+	
+	@Test
+	public void testNewOrder() {
+		//Comprobar que devuelva una orden vacía
+		o = C.newOrder();
+		assert(o.isEmpty());
+	}
+	
+	@Test
+	public void testAddProductToOrder() {
+		//Comprobar que se añade una orderline a la orden actual con el producto
+		C.addProductToOrder(o, p1);
+		List<OrderLine> lp = o.getProducts();
+		
+		OrderLine ol = lp.get(0);
+		assertEquals(ol.getProduct(), p1); //Necesitará el equals????
+		assert(ol.getAmount() == 1);
+	}
+	
+	@Test
+	public void testDeleteProductFromOrder() {
+		//Comprobar que se borra la orderline con el producto de la orden
+	}
+	
+	
+	
 }
