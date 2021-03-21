572cf5e41bfe60c20e0c6af9c1ec555f70144933
Jesega <jesega2000@gmail.com>
2021-03-21 21:30:45 +0100

    Comentadas las clases ICafe, Cafe y CashBox

--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/CashBox.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/CashBox.java
@@ -6,26 +6,49 @@ public class CashBox {
 	private int nOrders;
 	private BigDecimal total;
 	
+	/**
+	 * Return the number of registered orders
+	 * @return number of registered orders
+	 */
 	public int getnOrders() {
 		return nOrders;
 	}
 	
+	/**
+	 * Set the number of registered orders
+	 * @param nOrders number of orders to set
+	 */
 	public void setnOrders(int nOrders) {
 		this.nOrders = nOrders;
 	}
 	
+	/**
+	 * Increment in a unit the nnumber of registered orders
+	 */
 	public void incrementOrders() {
 		this.nOrders = getnOrders()+1;
 	}
 	
+	/**
+	 * Returns the total money earned
+	 * @return total money earned
+	 */
 	public BigDecimal getTotal() {
 		return total;
 	}
 	
+	/** 
+	 * Set the total money earned
+	 * @param total to set
+	 */
 	public void setTotal(BigDecimal total) {
 		this.total = total;
 	}
 	
+	/**
+	 * Increment the total money earned
+	 * @param inc amount to increase the total 
+	 */
 	public void addtoTotal(BigDecimal inc) {
 		this.total = total.add(inc);
 	}
--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/ICafe.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/ICafe.java
@@ -11,19 +11,57 @@ import java.util.List;
  */
 public interface ICafe {
 	
+	/**
+	 * Create a new order
+	 * @return the new ordenr created
+	 */
 	public Order newOrder();
-	//Productos disponibles
+	
+	/**
+	 * Returns a set of available products
+	 * @return List with the availables products
+	 */
 	public List<Product> getAvailableProducts();
-	//Añadir producto al pedido en curso
+	
+	/**
+	 * To add a unit of a product to an order
+	 * @param ord the order in which you want to add the product
+	 * @param p the product which you want to be added
+	 */
 	public void addProductToOrder(Order ord, Product p);
-	//Añadir producto al pedido en curso
+	
+	/**
+	 * To add several units of a product to an order
+	 * @param ord the order in which you want to add the product
+	 * @param p the product which you want to be added
+	 * @param c the quantity of the product
+	 */
 	public void addProductToOrder(Order ord, Product p, int c);
-	//Eliminar producto del pedido en curso
+	
+	/**
+	 * To delete a unit of a product from an order
+	 * @param ord the order in which you want to add the product
+	 * @param p the product which you want to be added
+	 */ 
 	public void deleteProductFromOrder(Order ord, Product p);
-	//Eliminar producto del pedido en curso
+	
+	/**
+	 * To delete several unit of a product from an order
+	 * @param ord the order in which you want to add the product
+	 * @param p the product which you want to be added
+	 * @param c the quantity of the product you wannt to delete 
+	 */
 	public void deleteProductFromOrder(Order ord, Product p, int c);
-	//Finalizar pedido
+	
+	/**
+	 * Register the order to finish it
+	 * @param ord the order you want to register
+	 */
 	public void FinishOrder(Order ord);
-	//Consultar caja del día 
+	
+	/**
+	 * Returs the amount of registered orders and the money earned
+	 * @return a CashBox with the amount of orders registered and the money earned
+	 */
 	public CashBox getCashBox();
 }
