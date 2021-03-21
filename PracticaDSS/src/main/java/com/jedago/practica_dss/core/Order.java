70975a5009faf152a60469ca2eadebb4cc51ed9f
gonzalouli <43575107+gonzalouli@users.noreply.github.com>
2021-03-21 22:12:01 +0100

    Javadoc introducido

--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/Order.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/Order.java
@@ -1,21 +1,29 @@
 package com.jedago.practica_dss.core;
 
 import java.math.BigDecimal;
+
 import java.util.*;
 
+/**
+ * Una clase para representar un pedido realizado en
+ * la clase cafe. El pedido esta determinado por una 
+ * id, una lista de OrderLine, un precio total
+ * del pedido y una fecha de apertura.
+ * @version 1.0. 21/03/2021
+ * @author Gonzalo Ulibarri
+ */
 public class Order implements Iterable<OrderLine>
 {
 	public static long currentid = 1;
 	private long id_order;
 	private List<OrderLine> OrderLineProduct;
-
-	//private enum payment_method {CARD,CASH};
 	private BigDecimal price;
 	private Date date;
-	//private enum status {OPEN, CLOSED, IN_PROCESS};
-
-//#########################################################
 	
+	 /** 
+     * Crea pedido vacío con un id unico, una lista
+     * de OrderLine vacía y una fecha determinada.
+     */
 	public Order(){
 		this.id_order = currentid;
 		currentid++;
@@ -25,32 +33,63 @@ public class Order implements Iterable<OrderLine>
 	}
 	
 //#########################################################
+	
+
+	 /** 
+     * Devuelve el id de un pedido determinado
+	 * @return El id del pedido.
+	 */
 	public long getId_order() {
 		return this.id_order;
 	}
+	
+	
+	 /** 
+     * Devuelve el precio de un pedido determinado.
+	 * @return El precio del pedido.
+	 */
 	public BigDecimal getPrice() {
 		return this.price;
 	}
 	
+	 /** 
+     * Comprueba la fecha en la que se creó el pedido.
+	 * @return El id del pedido.
+	 */
 	public Date getDate() {
 		return this.date;
 	}
-	
+
+ /** 
+ * Indica si el pedido está vacio.
+ * @return True si el pedido está vacio, false en caso contrario.
+ */
 	public boolean isEmpty() {
 		return OrderLineProduct.isEmpty();
 	}
-	
+
+ /** 
+ * Devuelve el tamaño de un pedido determinado.
+ * @return El tamaño de un pedido.
+ */
 	public int size() {
 		return OrderLineProduct.size();
 	}
 	
 	
-//#########################################################
-	public List<OrderLine> getProducts() 
+ /** 
+ * Devuelve la lista de OrderLine que contiene el producto.
+ * @return Lista de OrderLine del producto.
+	 */	public List<OrderLine> getProducts() 
 	{
 		return this.OrderLineProduct;
 	}
 	
+	 
+ /** 
+ * Introduce una OrderLine en el pedido.
+ * @param currentOrderLine OrderLine a introducir en el pedido.
+ */
 	public void setProducts(OrderLine currentOrderLine ) 
 	{	
 		this.OrderLineProduct.add(currentOrderLine);
@@ -59,9 +98,11 @@ public class Order implements Iterable<OrderLine>
 		this.price = this.price.add(total);
 	}
 
-//################ Operaciones Externas?#################
 	
-	
+	 /** 
+     * Introduce una unidad de un producto determinado en el pedido.
+	 * @param currentProduct define el producto que queremos introducir en el pedido.
+	 */
 	public void addProductToOrder(Product currentProduct)
 	{	
 		/*for(OrderLine pivot : OrderLineProduct) {
@@ -78,6 +119,11 @@ public class Order implements Iterable<OrderLine>
 		
 	}
 	
+	 /** 
+     * Introduce un determinado número de unidades del producto en el pedido.
+	 * @param currentProduct define el producto que queremos introducir en el pedido.
+	 * @param cant determina la cantidad de producto que queremos introducir.
+	 */
 	public void addProductToOrder(Product currentProduct , int cant)
 	{	
 		for(OrderLine pivot : OrderLineProduct) {
@@ -93,6 +139,13 @@ public class Order implements Iterable<OrderLine>
 		setProducts(ol);
 	}
 	
+	 /** 
+     * Elimina una cantidad determinada de un producto dado en el pedido. Si la cantidad 
+     * es mayor o igual a la cantidad actual del producto en el OrderLine se
+     * elimina el producto de la lista de OrderLine.
+	 * @param currentProduct define el producto que queremos eliminar del pedido.
+	 * @param cant cantidad del producto a eliminar.
+	 */
 	public void deleteProductFromOrder(Product currentProduct, int cant) {
 		
 		///for(OrderLine pivot : OrderLineProduct) {
@@ -121,19 +174,33 @@ public class Order implements Iterable<OrderLine>
 		}	
 	}
 	
+	 /** 
+     * Elimina una OrderLine determinada de la lista de OrderLine del pedido.
+	 * @param currentOrderLine define la OrderLine que queremos eliminar del pedido.
+	 */
 	public void deleteOrderlineFromOrder(OrderLine currentOrderLine) 
 	{
 		OrderLineProduct.remove(currentOrderLine);
 		
 	}
 	
+	/** 
+     * Elimina una OrderLine determinada de la lista de OrderLine del pedido utilizando un iterador.
+	 * @param it Iterador que establece una OrderLine determinada en la lista de
+	 * OrderLine.
+	 */
 	public void deleteOrderlineFromOrder(Iterator<OrderLine> it) 
 	{
 		it.remove();
 		
 	}
 	
-	@Override	
+	/** 
+     * Devuelve un iterador del tipo OrderLine.
+	 * @return Iterador de la lista OrderLineProduct.
+	 */
+	
+	@Override
 	public Iterator<OrderLine> iterator() {
 		return OrderLineProduct.iterator();
 	}
--- PracticaDSS/src/main/java/com/jedago/practica_dss/core/OrderLine.java
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/OrderLine.java
@@ -2,52 +2,96 @@ package com.jedago.practica_dss.core;
 
 import java.math.BigDecimal;
 
+/**
+ * Una clase para representar una linea de un producto
+ * dentro del pedido. Esta clase contiene un producto y su cantidad
+ * a refleja en la OrderLine.
+ * @version 1.0. 21/03/2021
+ * @author Gonzalo Ulibarri
+ */
+
 public class OrderLine {
 	
 	Product product_;
 	int amount;
 
-	public OrderLine(Product product_, int amount) {
+ /** 
+ * Crea pedido vacío con un id unico, una lista
+ * de OrderLine vacía y una fecha determinada.
+ * @param product_ producto  deseado a introducir en la OrderLine.
+ * @param amount cantidad a introducir del producto product_.
+ */
+	public OrderLine(Product product_, int amount) 
+	{
 		this.product_ = product_;
 		this.amount = amount;
 	}
 	
-	public Product getProduct() {
+ /** 
+ * Devuelve el producto que pertenede a una OrderLine determinada
+ * @return Producto de la OrderLine.
+ */
+	public Product getProduct() 
+	{
 		return product_;
 	}
 	
-	public void setProduct(Product newproduct) {
+
+ /** 
+ * Introduce un nuevo producto en una OrderLine determinada.
+ * @param newproduct nuevo producto a introducir.
+ */
+	public void setProduct(Product newproduct) 
+	{
 		product_ = newproduct;
 		
 	}
 	
-	public String getProductName() {
+ /** 
+ * Devuelve un string que representa el nombre del producto
+ * que pertenede a la una OrderLine determinada.
+ * @return Nombre del producto.
+ */	
+	public String getProductName() 
+	{
 		return product_.getName();
 	}
 
-	public int getAmount() {
+ /** 
+ * Devuelve la cantidad de producto product_ que tenemos
+ * en una OrderLine determinada.
+ * @return Cantidad de producto en la OrderLine.
+ */
+	public int getAmount() 
+	{
 		return amount;
 	}
 	
-	public void setAmount(int q){
-		assert(q>0);
+ /** 
+ * Introduce una nueva cantidad del producto product_ 
+ * en una OrderLine determinada.
+ * @param q Nueva cantidad del producto en la OrderLine.
+ */
+	public void setAmount(int q)
+	{
+		assert(q>=0);
 		
 		this.amount = q;
 		
-		// TODO Auto-generated method stub
-		
 	}
 
-	public BigDecimal getTotalPrice() {
+ /** 
+ * Devuelve el coste total de la cantidad de todos del 
+ * productos dentro de una OrderLine determinada.
+ * @return Precio total de la OrderLine.
+ */
+	public BigDecimal getTotalPrice() 
+	{
 		BigDecimal coste = BigDecimal.ZERO;
 		BigDecimal costetotal = BigDecimal.ZERO;
 		coste = product_.getPriceUnit().multiply(new BigDecimal(amount));
 		costetotal = costetotal.add(coste);
-		return costetotal;
-				
+		return costetotal;			
 	}
-
-
-	
 
 }
