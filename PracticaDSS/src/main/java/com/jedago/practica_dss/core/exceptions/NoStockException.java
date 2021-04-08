44453e900e54c9fcf4d409dfe9d44fa476b71460
Jesega <jesega2000@gmail.com>
2021-04-08 12:56:09 +0200

    Paquetes de excepción

---
+++ PracticaDSS/src/main/java/com/jedago/practica_dss/core/exceptions/NoStockException.java
@@ -0,0 +1,22 @@
+package com.jedago.practica_dss.core.exceptions;
+
+public class NoStockException extends Exception {
+
+	private static final long serialVersionUID = -4562782375354713185L;
+
+	public NoStockException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoStockException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoStockException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
