aa2463d62849f202aad749bcd722247125e5d94f
Jesega <jesega2000@gmail.com>
2021-04-20 16:27:39 +0200

    Corrección de pom.xml para añadir gson

--- Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
@@ -9,14 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jedago.practica_dss.*;
-import com.jedago.practica_dss.core.Cafe;
 import com.jedago.practica_dss.core.Client;
-import com.jedago.practica_dss.core.ICafe;
-import com.jedago.practica_dss.persistance.OrdersRepository;
-import com.jedago.practica_dss.persistance.OrdersRepositoryByFile;
-import com.jedago.practica_dss.persistance.ProductsRepository;
-import com.jedago.practica_dss.persistance.ProductsRepositoryByFile;
 
 @SpringBootApplication
 @RestController
