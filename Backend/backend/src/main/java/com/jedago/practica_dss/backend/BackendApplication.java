2e4cbb5054c7f9b9a3db01f15f77b36618fb4f69
gonzalouli <43575107+gonzalouli@users.noreply.github.com>
2021-04-20 20:08:14 +0200

    add user

--- Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
@@ -3,8 +3,16 @@ package com.jedago.practica_dss.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jedago.practica_dss.core.Cafe;
+import com.jedago.practica_dss.core.User;
+import com.jedago.practica_dss.persistance.OrdersRepositoryByFile;
+import com.jedago.practica_dss.persistance.ProductsRepositoryByFile;
+import com.jedago.practica_dss.persistance.UsersRepositoryByFile;
 
 @SpringBootApplication
 @RestController
@@ -14,10 +22,24 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 	
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+	@PostMapping("/users")
+	public void newUser(@RequestBody User newUser) throws Exception 
+	{
+		OrdersRepositoryByFile or = new OrdersRepositoryByFile();
+		ProductsRepositoryByFile pr = new ProductsRepositoryByFile();
+		UsersRepositoryByFile ur = new UsersRepositoryByFile();
+		
+		Cafe cafe =new Cafe(or, pr, ur);
+		
+		cafe.registerUser(newUser);
 	}
+	
+	
+	
+	
+	
+			
 
 }
--- Core/src/main/java/com/jedago/practica_dss/core/User.java
+++ Core/src/main/java/com/jedago/practica_dss/core/User.java
@@ -1,53 +1,119 @@
 package com.jedago.practica_dss.core;
 import java.time.LocalDate;
 import java.time.Period;
+/**@author Gonzalo Ulibarri Garcia
+ *@version 1.0
+ */
 
 public class User {
 	
+	public static long currentid_user = 1;
+
+	private long id_user;
 	private String firstName;
 	private String lastName;
 	private LocalDate birthDate;
 	private String dni;
 	
+	/** 
+     * Create an user;
+     * @param firstName The first name of an user
+     * @param lastName The last name of an user
+     * @param birthDate The birthDate of an user
+     * @param dni The dni of an user
+	 */
 	public User(String firstName, String lastName, LocalDate birthDate, String dni) {
+		
+		LocalDate now = LocalDate.now();
+		Period period = Period.between(birthDate, now);
+		
+		assert(period.getYears()>=18);
+		
+		this.id_user = currentid_user++;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.birthDate = birthDate;
 		this.dni = dni;
+		
 	}
-
+	
+	/** 
+     * Return the id of an user
+     * @return the id of the user
+	 */
+	public long getIdUser() 
+	{
+		return this.id_user;
+	}
+	
+	/** 
+     * Return the first name of an user
+     * @return the first name of the user
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/** 
+     * Set the first name of an existing user
+     * @param firstName the first name of the user
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/** 
+     * Return the last name of an user
+     * @return the last name of the user
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/** 
+     * Set the last name of an existing user
+     * @param lastName the first name of the user
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/** 
+     * Return the birth date of an user
+     * @return the birth date of the user
+	 */
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
 
+	/** 
+     * Set the birth date of an existing user
+     * @param birthDate the birth date of the user
+	 */
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
 
+	/** 
+     * Return the dni of an user
+     * @return the dni of the user
+	 */
 	public String getDni() {
 		return dni;
 	}
 
+	/** 
+     * Set the dni of an existing user
+     * @param dni the dni date of the user
+	 */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
 	
+	/** 
+     * Return the age of an user
+     * @return the age of the user
+	 */
 	public int getAge() {
 		
 		LocalDate now = LocalDate.now();
