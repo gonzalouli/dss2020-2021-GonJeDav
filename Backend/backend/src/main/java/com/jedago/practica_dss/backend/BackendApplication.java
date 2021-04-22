9a7b068f83fd8b1155a92cc9c5c2bb93c311e661
gonzalouli <43575107+gonzalouli@users.noreply.github.com>
2021-04-22 18:13:14 +0200

    Envio de email 0.1

--- Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/jedago/practica_dss/backend/BackendApplication.java
@@ -1,18 +1,24 @@
 package com.jedago.practica_dss.backend;
 
+import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.Properties;
+import com.jedago.practica_dss.backend.EnvioEmail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.jedago.practica_dss.core.Cafe;
+import com.jedago.practica_dss.core.CashBox;
 import com.jedago.practica_dss.core.Order;
 import com.jedago.practica_dss.core.Product;
 import com.jedago.practica_dss.core.ProductType;
@@ -20,7 +26,6 @@ import com.jedago.practica_dss.core.User;
 import com.jedago.practica_dss.persistance.OrdersRepositoryByFile;
 import com.jedago.practica_dss.persistance.ProductsRepositoryByFile;
 import com.jedago.practica_dss.persistance.UsersRepositoryByFile;
-
 @SpringBootApplication
 @RestController
 public class BackendApplication {
@@ -46,6 +51,14 @@ public class BackendApplication {
 		return cafe.getAvailableProducts(); 
 	}
 	
+	@GetMapping("/orders")
+	public List<Order> getAllOrders() throws Exception 
+	{
+		return cafe.getRegisteredOrders(); 
+	}
+	
+	
+	
 	@GetMapping("/productTypes")
 	public List<ProductType> getProductTypes() throws Exception 
 	{
@@ -65,6 +78,19 @@ public class BackendApplication {
 		return cafe.getRegisteredUsers(); 
 	}
 	
+	
+	@GetMapping("/users/orders/{iduser}")
+	public List<Order> getUserOrders(@PathVariable int iduser ) throws Exception 
+	{
+		User u = ur.findUserById(iduser);
+		return cafe.getUserOrders(u);
+		
+	}
+	
+	
+	
+	
+	
 	//RequestParam va en la URI
 	//RequestBody va en la petición HTTP
 	@PostMapping("/users")
@@ -79,6 +105,51 @@ public class BackendApplication {
 			
 			or.findById(idorder).setPickUpTime(ldt);
 	}
+	
+	@PostMapping("/order")
+	public void createOrder(@RequestBody Order order) 
+	{
+		
+	}
+	
+	@PostMapping("/order/product")
+	public void addProduct(@RequestBody(required=true) int idproduct, @RequestBody int cant, @RequestParam(required=true) int idorder ) 
+	{
+		
+	}
+	
+	@PostMapping("/order/delete")
+	public void deleteProduct(@RequestBody(required=true) int idproduct, @RequestBody(required=true) int cant,
+			@RequestParam(required=true, name="idorder") int idorder ) 
+	{
+		
+	}
+	
+	
+	@GetMapping("/cashbox")
+	public CashBox getCashBox(@PathVariable LocalDate ld) {
+		if(ld==null)
+			return cafe.getCashBox(LocalDate.now());
+		else
+			return cafe.getCashBox(ld);
+	}
+	
+	
+	
+	@PostMapping("/order/end")
+	public void finishOrder(@RequestBody(required=true) int idorder) throws Exception 
+	{
+		EnvioEmail mail = new EnvioEmail();
+		if( or.findById(idorder).isEmpty() ) 
+		{
+			Order order =  or.findById(idorder);
+			mail.sendEmail(order);
+		}
+			
+		
+	}
+	
+	
 	
 	
 			
