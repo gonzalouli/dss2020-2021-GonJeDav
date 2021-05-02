f6e40adee4474cb021c41c10766f30dacdfe2521
Jesega <jesega2000@gmail.com>
2021-05-02 21:19:24 +0200

    Método equals de user

--- Core/src/main/java/com/jedago/practica_dss/core/User.java
+++ Core/src/main/java/com/jedago/practica_dss/core/User.java
@@ -161,4 +161,28 @@ public class User implements Serializable {
 		
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_user == null) ? 0 : id_user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id_user == null) {
+			if (other.id_user != null)
+				return false;
+		} else if (!id_user.equals(other.id_user))
+			return false;
+		return true;
+	}
 }
