35fa55c4b4289f187020b726dc9e36611b79df64
Jesega <jesega2000@gmail.com>
2021-05-11 20:52:44 +0200

    Puesto como RestController

--- accessing-data-mysql/accessing-data-mysql/src/main/java/com/jedago/practica_dss/admysql/accessingdatamysql/MainController.java
+++ accessing-data-mysql/accessing-data-mysql/src/main/java/com/jedago/practica_dss/admysql/accessingdatamysql/MainController.java
@@ -1,5 +1,7 @@
 package com.jedago.practica_dss.admysql.accessingdatamysql;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller // This means that this class is a Controller
+@RestController // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
   @Autowired // This means to get the bean called userRepository
@@ -31,8 +34,8 @@ public class MainController {
   }
 
   @GetMapping(path="/all")
-  public @ResponseBody Iterable<User> getAllUsers() {
+  public List<User> getAllUsers() {
     // This returns a JSON or XML with the users
-    return userRepository.findAll();
+    return (List<User>) userRepository.findAll();
   }
 }
