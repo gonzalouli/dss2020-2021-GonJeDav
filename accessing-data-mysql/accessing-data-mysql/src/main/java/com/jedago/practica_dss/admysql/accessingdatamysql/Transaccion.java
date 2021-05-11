dad8c778c2538e98c15db1ca5c3093b2ac950b1c
Jesega <jesega2000@gmail.com>
2021-05-11 20:10:30 +0200

    Funcionara?

---
+++ accessing-data-mysql/accessing-data-mysql/src/main/java/com/jedago/practica_dss/admysql/accessingdatamysql/Transaccion.java
@@ -0,0 +1,96 @@
+package com.jedago.practica_dss.admysql.accessingdatamysql;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javassist.compiler.ast.Pair;
+@Entity
+public class Transaccion {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String id;
+	private String concepto;
+	private User usuario;
+	private LocalDateTime fecha;
+	private BigDecimal price;
+	
+	Transaccion(){super();}
+	
+	Transaccion(User user, String concepto, LocalDateTime fecha, BigDecimal pay){
+		this.usuario= user;
+		this.concepto= concepto;
+		this.fecha = fecha;
+		this.setPrice(pay);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the concepto
+	 */
+	public String getConcepto() {
+		return concepto;
+	}
+
+	/**
+	 * @param concepto the concepto to set
+	 */
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+
+	/**
+	 * @return the usuario
+	 */
+	public User getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(User usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the fecha
+	 */
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	/**
+	 * @param fecha the fecha to set
+	 */
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+
+}
